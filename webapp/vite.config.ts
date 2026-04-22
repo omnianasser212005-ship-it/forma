8ad6b4d8254b9a315f010ed27a7e57a589cfe807
unknown <omnianasser212005@gmail.com>
2026-04-22 02:01:16 +0200

    Save local changes

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/forma/', // غير إلى اسم مستودعك على GitHub
+  base: '/', // غير إلى اسم مستودعك على GitHub
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
